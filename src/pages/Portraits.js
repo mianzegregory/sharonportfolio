1f7308634a99ab0051245773c1a29ba10ddcaae0
Gregory Mianze <mianzegregory@outlook.com>
2021-07-15 11:32:27 +0300

    use image component to display images on portraits page

--- src/pages/Portraits.js
+++ src/pages/Portraits.js
@@ -1,24 +1,26 @@
 import React from "react";
 import styled from "styled-components";
+import Image from "../components/common/Image";
 import data from "../data.json";
 
 const Portraits = () => {
   return (
     <Wrapper>
-      <img src={data.images.portraits[1]} />
-      <img src={data.images.portraits[2]} />
-      <img src={data.images.portraits[3]} />
-      <img src={data.images.portraits[4]} />
-      <img src={data.images.portraits[5]} />
-      <img src={data.images.portraits[6]} />
-      <img src={data.images.portraits[7]} />
-      <img src={data.images.portraits[8]} />
-      <img src={data.images.portraits[9]} />
-      <img src={data.images.portraits[10]} />
-      <img src={data.images.portraits[11]} />
-      <img src={data.images.portraits[12]} />
-      <img src={data.images.portraits[13]} />
-      <img src={data.images.portraits[14]} />
+      <Image image={data.images.portraits[1]} />
+      <Image image={data.images.portraits[1]} />
+      <Image image={data.images.portraits[2]} />
+      <Image image={data.images.portraits[3]} />
+      <Image image={data.images.portraits[4]} />
+      <Image image={data.images.portraits[5]} />
+      <Image image={data.images.portraits[6]} />
+      <Image image={data.images.portraits[7]} />
+      <Image image={data.images.portraits[8]} />
+      <Image image={data.images.portraits[9]} />
+      <Image image={data.images.portraits[10]} />
+      <Image image={data.images.portraits[11]} />
+      <Image image={data.images.portraits[12]} />
+      <Image image={data.images.portraits[13]} />
+      <Image image={data.images.portraits[14]} />
     </Wrapper>
   );
 };
