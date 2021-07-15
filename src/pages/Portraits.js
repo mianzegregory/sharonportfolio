2044dbaf4e80d8f25d0b25be0fc5dc11d429aabe
Gregory Mianze <mianzegregory@outlook.com>
2021-07-15 12:40:21 +0300

    layout portraits page

--- src/pages/Portraits.js
+++ src/pages/Portraits.js
@@ -1,28 +1,83 @@
 import React from "react";
-import PageWrapper from "../components/common/PageWrapper";
-import Image from "../components/common/Image";
 import data from "../data.json";
+import Gallery from "react-photo-gallery";
+import PageWrapper from "../components/common/PageWrapper";
 
 const Portraits = () => {
-  return (
-    <PageWrapper>
-      <Image image={data.images.portraits[1]} />
-      <Image image={data.images.portraits[1]} />
-      <Image image={data.images.portraits[2]} />
-      <Image image={data.images.portraits[3]} />
-      <Image image={data.images.portraits[4]} />
-      <Image image={data.images.portraits[5]} />
-      <Image image={data.images.portraits[6]} />
-      <Image image={data.images.portraits[7]} />
-      <Image image={data.images.portraits[8]} />
-      <Image image={data.images.portraits[9]} />
-      <Image image={data.images.portraits[10]} />
-      <Image image={data.images.portraits[11]} />
-      <Image image={data.images.portraits[12]} />
-      <Image image={data.images.portraits[13]} />
-      <Image image={data.images.portraits[14]} />
-    </PageWrapper>
-  );
+  const photos = [
+    {
+      src: data.images.portraits[7],
+      width: 4,
+      height: 3,
+    },
+    {
+      src: data.images.portraits[8],
+      width: 4,
+      height: 4,
+    },
+    {
+      src: data.images.portraits[9],
+      width: 6,
+      height: 4,
+    },
+    {
+      src: data.images.portraits[10],
+      width: 6,
+      height: 4,
+    },
+    {
+      src: data.images.portraits[11],
+      width: 4,
+      height: 3,
+    },
+    {
+      src: data.images.portraits[12],
+      width: 6,
+      height: 4,
+    },
+    {
+      src: data.images.portraits[13],
+      width: 3,
+      height: 5,
+    },
+    {
+      src: data.images.portraits[14],
+      width: 3,
+      height: 5,
+    },
+    {
+      src: data.images.portraits[1],
+      width: 4,
+      height: 3,
+    },
+    {
+      src: data.images.portraits[2],
+      width: 4,
+      height: 3,
+    },
+    {
+      src: data.images.portraits[3],
+      width: 4,
+      height: 3,
+    },
+    {
+      src: data.images.portraits[4],
+      width: 4,
+      height: 4,
+    },
+    {
+      src: data.images.portraits[5],
+      width: 4,
+      height: 4,
+    },
+    {
+      src: data.images.portraits[6],
+      width: 4,
+      height: 4,
+    },
+  ];
+
+  return <Gallery photos={photos} direction={"column"} />;
 };
 
 export default Portraits;
