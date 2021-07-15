115c45ad54989b2900de95a3919f78d0e04f6525
Gregory Mianze <mianzegregory@outlook.com>
2021-07-15 11:09:30 +0300

    add lazy loading dependency

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Portraits from "./pages/Portraits";
+import Events from "./pages/Events";
+import Flatlays from "./pages/Flatlays";
+import Shoots from "./pages/Shoots";
 
 function App() {
   return (
     <div className="App">
       <Portraits />
+      {/* <Events /> */}
+      {/* <Flatlays /> */}
+      {/* <Shoots /> */}
     </div>
   );
 }
