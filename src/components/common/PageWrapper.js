ad8f76ded311702357361ded3aec03cc49cf0c59
Gregory Mianze <mianzegregory@outlook.com>
2021-07-15 11:38:49 +0300

    create reusable pagewrapper component

---
+++ src/components/common/PageWrapper.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const PageWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+  margin: 0;
+  width: 100vw;
+  min-height: 100vh;
+`;
+
+export default PageWrapper;
