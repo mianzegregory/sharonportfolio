0e2dad42e1cf914919c7eb25d0e68686cbb4e700
Gregory Mianze <mianzegregory@outlook.com>
2021-07-15 13:14:47 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import Portraits from "./pages/Portraits";
 import Events from "./pages/Events";
 import Flatlays from "./pages/Flatlays";
 import Shoots from "./pages/Shoots";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Slide } from "@material-ui/core";
 
 function App() {
-  return (
-    <div className="App">
-      <Portraits />
-      {/* <Events /> */}
-      {/* <Flatlays /> */}
-      {/* <Shoots /> */}
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 900);
+    // AOS.init({
+    //   duration: 1000,
+    // });
+    // AOS.refresh();
+  }, []);
+
+  return loading === false ? (
+    <Router>
+      <Slide direction="up" in>
+        <Switch>
+          <Route path="/" exact component={Portraits} />
+          <Route path="/events" exact component={Events} />
+          <Route path="/flatlays" exact component={Flatlays} />
+          <Route path="/shoots" exact component={Shoots} />
+        </Switch>
+      </Slide>
+    </Router>
+  ) : (
+    <div>
+      <p>Loading</p>
     </div>
   );
 }
