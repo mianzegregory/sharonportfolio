4989d19a8ebb83d9e203c3a925519bf0566b304a
Gregory Mianze <41749350+mianzegregory@users.noreply.github.com>
2021-09-13 14:53:34 +0300

    create new empty pages & remove old ones

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PageWrapper from "../components/common/PageWrapper";
+
+const Portfolio = () => {
+  return <PageWrapper></PageWrapper>;
+};
+
+export default Portfolio;
