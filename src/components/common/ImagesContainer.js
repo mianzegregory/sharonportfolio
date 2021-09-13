40a3f58948505e79d29b9bc29b8ec2cdbbc69b7c
Gregory Mianze <41749350+mianzegregory@users.noreply.github.com>
2021-09-13 17:15:16 +0300

    modify image container component to take in six images

--- src/components/common/ImagesContainer.js
+++ src/components/common/ImagesContainer.js
@@ -14,4 +14,25 @@ const ImagesContainer = styled.div`
   }
 `;
 
+export const SixImagesContainer = styled.div`
+  background-color: #f3ebe5;
+  margin-top: 5rem;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 85%;
+  border-radius: 0.3rem;
+  .lazy-load-image-background.blur.lazy-load-image-loaded {
+    background-color: transparent;
+  }
+  .img-row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    background-color: transparent;
+  }
+`;
+
 export default ImagesContainer;
