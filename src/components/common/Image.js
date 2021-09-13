f3a4a8e1d9024e7374190f14f4156a7f9e991603
Gregory Mianze <41749350+mianzegregory@users.noreply.github.com>
2021-09-13 17:35:56 +0300

    implement lightbox functionality to allow images to be opened when clicked

--- src/components/common/Image.js
+++ src/components/common/Image.js
@@ -3,11 +3,26 @@ import styled from "styled-components";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import placeholder from "../../assets/placeholder.webp";
+import { SRLWrapper } from "simple-react-lightbox";
 
 const Image = ({ src, alt }) => {
+  const options = {
+    buttons: {
+      backgroundColor: "#1b5245",
+      iconColor: "rgba(126, 172, 139, 0.8)",
+    },
+  };
+
   return (
     <ImgWrapper>
-      <Wrapper src={src} effect="blur" placeholderSrc={placeholder} alt={alt} />
+      <SRLWrapper options={options}>
+        <Wrapper
+          src={src}
+          effect="blur"
+          placeholderSrc={placeholder}
+          alt={alt}
+        />
+      </SRLWrapper>
     </ImgWrapper>
   );
 };
@@ -26,6 +41,9 @@ const ImgWrapper = styled.div`
     clip-path: ellipse(0.3rem 0.3rem 0.3rem 0.3rem);
     box-shadow: 0px 0px 5px 0 rgba(0, 0, 0, 0.1);
     border-radius: 0.3rem;
+  }
+  * {
+    background-color: transparent !important;
   }
 `;
 
