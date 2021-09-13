cbce5438957763bf39fdbd8ab81474112146563f
Gregory Mianze <41749350+mianzegregory@users.noreply.github.com>
2021-09-13 15:59:31 +0300

    modify reusable image component to fit new design

--- src/components/common/Image.js
+++ src/components/common/Image.js
@@ -4,16 +4,34 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import placeholder from "../../assets/placeholder.webp";
 
-const Image = ({ image, alt }) => {
+const Image = ({ src, alt }) => {
   return (
-    <Wrapper src={image} effect="blur" placeholderSrc={placeholder} alt={alt} />
+    <ImgWrapper>
+      <Wrapper src={src} effect="blur" placeholderSrc={placeholder} alt={alt} />
+    </ImgWrapper>
   );
 };
 
 export default Image;
 
-const Wrapper = styled(LazyLoadImage)`
-&& {
+const ImgWrapper = styled.div`
   width: 30%;
-}
+  height: auto;
+  object-fit: cover;
+  background-color: transparent;
+  img {
+    height: 25rem;
+    width: 15rem;
+    object-fit: cover;
+    clip-path: ellipse(0.3rem 0.3rem 0.3rem 0.3rem);
+    box-shadow: 0px 0px 5px 0 rgba(0, 0, 0, 0.1);
+    border-radius: 0.3rem;
+  }
+`;
+
+const Wrapper = styled(LazyLoadImage)`
+  && {
+    width: 100%;
+    margin: 0.5rem;
+  }
 `;
