04c2a46823b9054bd93665dda7b307886356c9c7
Gregory Mianze <41749350+mianzegregory@users.noreply.github.com>
2021-09-14 12:58:44 +0300

    develop shoots & events subpages

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,11 +1,13 @@
 import PageWrapper from "../components/common/PageWrapper";
 import Portraits from "./Portfolio/Portraits";
 import Flatlays from "./Portfolio/Flatlays";
+import Events from "./Portfolio/Events";
+import Shoots from "./Portfolio/Shoots";
 
 const Portfolio = () => {
   return (
     <PageWrapper>
-      <Flatlays />
+      <Shoots />
     </PageWrapper>
   );
 };
---
+++ src/pages/Portfolio/Shoots.js
@@ -0,0 +1,39 @@
+import Image from "../../components/common/Image";
+import { SixImagesContainer } from "../../components/common/ImagesContainer";
+
+const Shoots = () => {
+  return (
+    <SixImagesContainer>
+      <div className="img-row">
+        <Image
+          src="https://ik.imagekit.io/sgmianze96/Sharon_s_Portfolio/shoots_compressed/firr_6N9Tff8ZW.jpg?updatedAt=1626347265472"
+          alt="Shoot 1"
+        />
+        <Image
+          src="https://ik.imagekit.io/sgmianze96/Sharon_s_Portfolio/portraits_compressed/a_min_idn0zcqmn_rsBpKgaFZ.jpg?updatedAt=1626348852318"
+          alt="Shoot 2"
+        />
+        <Image
+          src="https://ik.imagekit.io/sgmianze96/Sharon_s_Portfolio/shoots_compressed/power_walk_0uAV54HyOu.jpg?updatedAt=1626347411452"
+          alt="Shoot 3"
+        />
+      </div>
+      <div className="img-row">
+        <Image
+          src="https://ik.imagekit.io/sgmianze96/Sharon_s_Portfolio/shoots_compressed/nimu_1_-r4adUqhss.jpg?updatedAt=1626347556850"
+          alt="Shoot 4"
+        />
+        <Image
+          src="https://ik.imagekit.io/sgmianze96/Sharon_s_Portfolio/shoots_compressed/nimu_2_HyCqP10NT.jpg?updatedAt=1626347719555"
+          alt="Shoot 5"
+        />
+        <Image
+          src="https://ik.imagekit.io/sgmianze96/Sharon_s_Portfolio/shoots_compressed/dont_like_8oAHD1c52.jpg?updatedAt=1626348187264"
+          alt="Shoot 6"
+        />
+      </div>
+    </SixImagesContainer>
+  );
+};
+
+export default Shoots;
