d6b35a3356a9d930b5df4c9f6853d4eb1e5d2061
Gregory Mianze <41749350+mianzegregory@users.noreply.github.com>
2021-09-14 13:24:09 +0300

    create buttons to render different subpages

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import PageWrapper from "../components/common/PageWrapper";
 import Portraits from "./Portfolio/Portraits";
 import Flatlays from "./Portfolio/Flatlays";
@@ -5,9 +6,24 @@ import Events from "./Portfolio/Events";
 import Shoots from "./Portfolio/Shoots";
 
 const Portfolio = () => {
+  const [subpage, setSubpage] = useState("portait");
+
+  // useEffect(() => {
+  //   setSubpage("portrait");
+  // });
+
   return (
     <PageWrapper>
-      <Portraits />
+      <div className="portfolio-routing-buttons">
+        <button onClick={() => setSubpage("portrait")}>Portraits</button>
+        <button onClick={() => setSubpage("flatlay")}>Flatlays</button>
+        <button onClick={() => setSubpage("event")}>Events</button>
+        <button onClick={() => setSubpage("shoot")}>Shoots</button>{" "}
+      </div>
+      {subpage === "portrait" && <Portraits />}
+      {subpage === "flatlay" && <Flatlays />}
+      {subpage === "event" && <Events />}
+      {subpage === "shoot" && <Shoots />}
     </PageWrapper>
   );
 };
